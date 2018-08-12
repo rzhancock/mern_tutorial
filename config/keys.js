91627e511787ca16737688079db2f15c5aaf5ccb
rzhancock <rzhancock@gmail.com>
2018-08-11 21:29:58 -0700

    MERN stack tutorial by Brad Traversy

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb://rzhancock:MERNstack1@ds125021.mlab.com:25021/mern-stack-tutorial'
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//bodyParser middleware
+app.use(bodyParser.json());
+
+//db config
+const db = require('./config/keys').mongoURI;
+
+//connect to mongo
+mongoose.connect(db)
+	.then(() => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err));
+
+	//Use Routes
+app.use('/api/items', items);
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+	});
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
