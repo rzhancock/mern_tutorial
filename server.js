df15b5eeb01084931be501032a23169a88e2b668
rzhancock <rzhancock@gmail.com>
2018-10-16 01:46:47 -0700

    production build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -24,7 +25,7 @@ if(process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
